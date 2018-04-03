8e6483370cbed6364b1436c8a125813d7d67304b
Alexandria Brown <axbn2190@gmail.com>
2018-04-02 22:19:41 -0700

    fixed edge cases and removed debugging print statements

--- lib/integer_palindrome_check.rb
+++ lib/integer_palindrome_check.rb
@@ -1,4 +1,23 @@
 # Returns true if the input positive integer number forms a palindrome. Returns false otherwise.
 def is_palindrome(number)
-  raise NotImplementedError
+  n = number
+  reverse = 0
+
+  if number == nil
+    return false
+  elsif number >= 0 && number <= 10
+    return true
+  end
+
+  while number > 0
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number = number / 10
+  end
+
+  if n == reverse
+    return true
+  else
+    return false
+  end
 end
